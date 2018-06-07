d421395c0123d7138badde4443c1d32253545d95
Mark Robles <39225230+markroblesphp@users.noreply.github.com>
2018-06-07 07:48:11 -0500

    Update problema.php

--- problema.php
+++ problema.php
@@ -13,11 +13,11 @@ define('COOKIE_FILE', $dir . '/cookies/' . md5(rand(100203,9999999999)) . '.txt'
 
 
 if(empty($_POST["account"])){
-	echo "<center>Por Favor Ingrese Usuario!!</center>";
-	exit;
+    echo "<center>Por Favor Ingrese Usuario!!</center>";
+    exit;
 }
 else{
-	check($_POST["account"]);
+    check($_POST["account"]);
 }
 
 
@@ -25,7 +25,7 @@ else{
 
 function check($acct)
 {
-			 $ch2 = curl_init();
+             $ch2 = curl_init();
 
 
 
@@ -49,7 +49,10 @@ $arr = explode('/', $matches[1]);
 $string = implode('/',array_slice($arr, 8,12));
 $string2 = "https://www.banorte.com/wps/portal/ixe/Home/acceso-a-banco-en-linea/".$string;
 
-			 $ch = curl_init();
+
+
+
+             $ch = curl_init();
 
 
 
@@ -71,14 +74,14 @@ $adressfake = getdatarand();
  
     $postFields['userField'] = $acct;
     $postFields['Image1.x'] = "41";
-	$postFields['Image1.y'] = "9";
+    $postFields['Image1.y'] = "9";
     $postFields['service'] = "direct/1/PortalLogin/form.form";
     $postFields['sp'] = "S0";
     $postFields['Form0'] = 'userField,$FormConditional,$ImageSubmit';
     $postFields['$FormConditional'] = "T";
     
 
-	
+    
 $authaccount = getPage($ch, "https://nixe.ixe.com.mx/NBXI/AALoginixe.aspx?banco=IXE", $postFields, $string2);
 
 $dataextra = getauth($authaccount);
@@ -86,9 +89,9 @@ preg_match("'<input name=\"userField\" type=\"hidden\">'si", $authaccount, $auth
 preg_match("'<span id=\"lblFrase\">(.*?)</span></div>'si", $authaccount, $frase);
 preg_match("'</span></span><span id=\"lblNombre\" class=\"Amarillo\" style=\"font-weight:bold;\">(.*?)</span>'si", $authaccount, $name);
 if($authyesorno){
-	echo "<center><h1>USUARIO NO ENCONTRADO!</h1></center>";
-	 curl_close($ch);
-	  curl_close($ch2);
+    echo "<center><h1>USUARIO NO ENCONTRADO!</h1></center>";
+     curl_close($ch);
+      curl_close($ch2);
 }
 else{
 if($frase){
@@ -105,34 +108,34 @@ curl_close($ch);
 }
 
 
-function getdatarand(){
-$f = file("dir19042017.txt");
-$string = $f[rand(0, count($f) - 1)];
-$arr = explode('|', $string);
-$arr2 = explode('|', $string);
+// function getdatarand(){
+// $f = file("dir19042017.txt");
+// $string = $f[rand(0, count($f) - 1)];
+// $arr = explode('|', $string);
+// $arr2 = explode('|', $string);
 
-$fname_rnd = implode('|',array_slice($arr, 1, 1));
-$lname_rnd = implode('|',array_slice($arr, 2, 1));
-$street_rnd = implode('|',array_slice($arr2, 3, 1));
-$mail_rnd = implode('|',array_slice($arr2, 4, 1));
-$phone = implode('|',array_slice($arr2, 5, 1));
-$zip = implode('|',array_slice($arr2, 6, 1));
-$city_rnd = implode('|',array_slice($arr2, 7, 1));
+// $fname_rnd = implode('|',array_slice($arr, 1, 1));
+// $lname_rnd = implode('|',array_slice($arr, 2, 1));
+// $street_rnd = implode('|',array_slice($arr2, 3, 1));
+// $mail_rnd = implode('|',array_slice($arr2, 4, 1));
+// $phone = implode('|',array_slice($arr2, 5, 1));
+// $zip = implode('|',array_slice($arr2, 6, 1));
+// $city_rnd = implode('|',array_slice($arr2, 7, 1));
 
-$info = array($fname_rnd, $lname_rnd, $street_rnd, $zip, $city_rnd);
+// $info = array($fname_rnd, $lname_rnd, $street_rnd, $zip, $city_rnd);
 
-return $info;
+// return $info;
 
-}
+// }
 
 function getnewexpmoth($expiram){
-	if ($expmonth<10){
-		$expm = substr($expiram,1,2);
-	}
-	else{
-		$expm = $expiram;
-	}
-	return $expm;
+    if ($expmonth<10){
+        $expm = substr($expiram,1,2);
+    }
+    else{
+        $expm = $expiram;
+    }
+    return $expm;
 }
 
 function validateChecksum($number) {
@@ -183,23 +186,23 @@ foreach($dom->getElementsByTagName('img') as $input) {
  if ($input->getAttribute("id") == "imgImagen") {
         $base64img = $input->getAttribute('src');
 
-		 }
+         }
 }
 foreach($dom->getElementsByTagName('input') as $input) {
 
  if ($input->getAttribute("name") == "__VIEWSTATEGENERATOR") {
         $viewstategen = $input->getAttribute('value');
 
-		 }
+         }
  if ($input->getAttribute("name") == "__EVENTVALIDATION") {
         $eventvalid = $input->getAttribute('value');
 
-		 }
+         }
  if ($input->getAttribute("name") == "__VIEWSTATE") {
         $viewstate = $input->getAttribute('value');
 
-		 }		 
-		 
+         }       
+         
 }
 $info = array($base64img,$viewstategen,$eventvalid,$viewstate);
 
@@ -210,10 +213,10 @@ return $info;
 
 function getPage(&$ch , $url , $postFields=null, $referer=null)
 {
-	if ($referer != null){
-		curl_setopt($ch, CURLOPT_REFERER, $referer);
-	}
-	
+    if ($referer != null){
+        curl_setopt($ch, CURLOPT_REFERER, $referer);
+    }
+    
       if($postFields != null)
     {
         $postString = "";
@@ -222,16 +225,15 @@ function getPage(&$ch , $url , $postFields=null, $referer=null)
         }
 
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $+);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
     }
-    curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-    curl_setopt($ch, CURLOPT_PROXY, "localhost:1071");
-    curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
+    // curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+    // curl_setopt($ch, CURLOPT_PROXY, "localhost:1071");
+    // curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, 1);
     curl_setopt($ch, CURLOPT_URL, $url);
     $result = curl_exec($ch);
     return $result;
-	
+    
     }
 
 ?>
-
